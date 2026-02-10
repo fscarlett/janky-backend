73faff1030cf741dd12dce16b9543bce5b30e827
Fox Scarlett <foxontheone@gmail.com>
2026-02-09 21:55:56 -0800

    player status model

---
+++ src/models/PlayerStatusModel.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+
+const scoreSchema = new Schema({
+  name: 'string',
+  level: 'number',
+  money: 'number',
+  jank: 'number',
+  cred: 'number',
+})
+
+const gearSchema = new Schema({
+  name: 'string',
+  type: 'string',
+  image: 'string',
+  description: 'string',
+  price: 'number',
+  cool_level: 'number',
+  stockedBy: ['number'],
+})
+
+const playerStatusSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    online: { type: Boolean, default: false },
+    lastActive: { type: Date, default: Date.now },
+    score: scoreSchema,
+    gear: [gearSchema],
+  },
+  {
+    timestamps: true,
+  },
+)
+
+const PlayerStatus = mongoose.model('PlayerStatus', playerStatusSchema)
+
+export default PlayerStatus
